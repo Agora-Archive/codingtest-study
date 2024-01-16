44a77d0dbd26741ac967493862a073ed30eea347
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-16 22:31:20 +0900

    [Add] WEEK11

--- seunghyun/[WEEK11] 과제 제출/1967. 여행가자/여행가자DFS.cpp
+++ seunghyun/[WEEK11] 과제 제출/1967. 여행가자/여행가자DFS.cpp
@@ -3,29 +3,21 @@
 using namespace std;
 
 int city[201][201] = {0};
+bool visited[201];
 int n;
 
-bool search(int trip, int next_trip, bool visited[201]) {
 
-    if (trip == next_trip) //현재 위치와 다음 위치가 같음
-        return true;
-
-    if (city[trip][next_trip] == 1)  //현재 위치와 다음 위치가 이어져있음
-        return true;
-
-    for (int i = 1; i <= n; i++) { //모든 경우를 살펴봄
-        if (city[trip][i] == 1 && !visited[i]) { //현재 위치와 이어진 곳이 방문되지 않았으면 재귀로 살펴봄
-            visited[i] = true;
-            if (search(i, next_trip, visited))
-                return true;
-        }
+void DFS(int node) {
+    visited[node] = true; //방문처리
+    for (int i = 1; i <= n; i++) {
+        if (city[node][i] == 1 && !visited[i]) //도시가 연결되어 있고 방문되지 않았다면 DFS
+            DFS(i);
     }
-    return false;
 }
 
 
 int main() {
-    int m, trip = 0, next_trip;
+    int m, next_trip;
     bool fail = false;
     cin >> n >> m;
     for (int i = 1; i <= n; i++) {
@@ -33,15 +25,15 @@ int main() {
             cin >> city[i][j];
         }
     }
-    for (int i = 0; i < m; i++) {
-        bool visited[201] = {false};
+    cin >> next_trip;
+    DFS(next_trip); //도시 연결 파악
+
+    for (int i = 1; i < m; i++) {
         cin >> next_trip;
-        if (trip != 0) {
-            if (!search(trip, next_trip, visited)) {
-                fail = true;
-            }
+        if (!visited[next_trip]) { //방문되지 않았으면 연결되지 않은 것이므로 종료
+            fail = true;
+            break;
         }
-        trip = next_trip;
     }
     if (fail)
         cout << "NO";
