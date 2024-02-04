74daf831121a4d5698aed0b6bc2bf08143d8401b
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-02-04 16:57:51 +0900

    [UPDATE] 2579.java 리뷰 반영 후 수정

--- sunan/백준 2579번_계단오르기/2579.java
+++ sunan/백준 2579번_계단오르기/2579.java
@@ -3,27 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static Integer []dp ;
+    static int []dp ;
     static int []score;
     public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         score = new int[n+1];
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++)
             score[i]= Integer.parseInt(br.readLine());
-        }
-        dp = new Integer[n+1];
-        dp[0] = score[0];
-        dp[1]= score[1];
-        if(n>1){
-            dp[2]= score[1]+score[2];
-        }
+        dp = new int[n+1];
         System.out.print(find(n));
     }
     static int find(int n){
-        if(dp[n]==null){
+        if(n < 1) return 0;
+        if(dp[n]==0)
             dp[n] = Math.max(find(n-2), find(n-3)+score[n-1])+score[n];
-        }
         return dp[n];
     }
 }
