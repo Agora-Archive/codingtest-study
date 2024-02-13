ea6c0b64d1ac2265916c04a2e5d4c3dd47dd7630
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-02-13 16:46:11 +0900

    [UPDATE] 2606.java 피드백 반영

--- sunan/백준 2606번_바이러스/2606.java
+++ sunan/백준 2606번_바이러스/2606.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static LinkedList<Integer> [] list;
@@ -13,7 +8,8 @@ public class Main {
     public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); //노드 개수
-        int m = Integer.parseInt(br.readLine());//간선 개수
+        //간선 개수
+        int m = Integer.parseInt(br.readLine());
         //전역변수 초기화
         list = new LinkedList[n+1];
         visited = new boolean[n+1];
@@ -27,12 +23,8 @@ public class Main {
             list[front].add(back);
             list[back].add(front);
         }
-        //정확한 탐색을 위한 정렬
-        for(int i=1; i<=n; i++){
-            Collections.sort(list[i]);
-        }
         DFS(1);
-        System.out.print(cnt-1);//1번 컴퓨터는 빼야
+        System.out.print(cnt-1);
     }
     static void DFS(int v){
         cnt++;
